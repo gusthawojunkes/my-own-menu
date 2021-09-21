6a14e39349ccebd0966ed4c45bf3b6ca0a776f4c
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2021-09-20 22:05:32 -0300

    WIP

--- lib/src/welcome/repositories/WelcomePage.dart
+++ lib/src/welcome/repositories/WelcomePage.dart
@@ -24,15 +24,44 @@ class WelcomePage extends StatefulWidget {
 class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-       body: Column(children: [
+    var _username = 'Usuário';
+    return Scaffold(
+      body: Column(children: [
         new Container(
           child: Image.asset(
             './lib/assets/BackgroundTop.png',
             fit: BoxFit.fill,
           ),
         ),
+        Text(
+          'Olá, $_username',
+          textAlign: TextAlign.start,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+        ),
+        Center(
+          child: Column(
+            children: <Widget> [
+              Container(
+                decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.grey
+              ),
+              margin: const EdgeInsets.all(10.0),
+              width: 48.0,
+              height: 48.0,
+              )
+            ],
+          ),
+        ),
+        Divider(
+          height: 35,
+          thickness: 2,
+          indent: 20,
+          endIndent: 20,
+          color: Colors.grey,
+        ),
       ]),
-     );
+    );
   }
 }
