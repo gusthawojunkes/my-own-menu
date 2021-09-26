caeb9c1455f71855ec866378d4f64216fa04bbc5
Nathan Stüber Antunes <nathan.stuber.a@gmail.com>
2021-09-26 09:51:41 -0300

    added sidebar to all pages that need it

--- lib/src/home/repositories/HomePage.dart
+++ lib/src/home/repositories/HomePage.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:myownmenu/src/recipe/repositories/RecipePage.dart';
 import 'package:myownmenu/src/notification/repositories/NotificationPage.dart';
 import 'package:myownmenu/src/welcome/repositories/WelcomePage.dart';
+import 'package:myownmenu/src/configuration/repositories/ConfigurationPage.dart';
+import 'package:myownmenu/src/dispense/repositories/DispensePage.dart';
+import 'package:myownmenu/src/ingredient/repositories/IngredientPage.dart';
+import 'package:myownmenu/src/login/repositories/LoginPage.dart';
+import 'package:myownmenu/src/profile/repositories/ProfilePage.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -26,12 +31,83 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
-  final List _options = [WelcomePage(), RecipePage(), NotificationPage()];
-
+  int _selectedIndex = 0;
+  dynamic _selectedDestination;
+  final List? _options = [
+    WelcomePage(),
+    RecipePage(),
+    NotificationPage(),
+    Dispense(),
+    Ingredient(),
+    Configuration(),
+    Profile(),
+    Login()
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _options[_currentIndex],
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        shadowColor: Colors.white12,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            new Padding(
+                padding: const EdgeInsets.only(top: 90),
+                child: TextButton(
+                  child: const Icon(Icons.arrow_back_ios),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )),
+            new Padding(
+                padding: const EdgeInsets.only(top: 45, left: 20),
+                child: Column(
+                  children: [
+                    ListTile(
+                      leading: Icon(Icons.featured_play_list),
+                      title: Text('Dispensa'),
+                      selected: _selectedDestination == 0,
+                      onTap: () => {sideBarDestination(3)},
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.local_cafe),
+                      title: Text('Ingredientes'),
+                      selected: _selectedDestination == 1,
+                      onTap: () => {sideBarDestination(4)},
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.build),
+                      title: Text('Configurações'),
+                      selected: _selectedDestination == 2,
+                      onTap: () => {sideBarDestination(5)},
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.account_circle),
+                      title: Text('Perfil'),
+                      selected: _selectedDestination == 3,
+                      onTap: () => {sideBarDestination(6)},
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.input),
+                      title: Text('Sair'),
+                      selected: _selectedDestination == 4,
+                      onTap: () => {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Login()),
+                        )
+                      },
+                    ),
+                  ],
+                )),
+          ],
+        ),
+      ),
+      body: _options![_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         onTap: onTabTapped,
         currentIndex: _currentIndex,
@@ -56,6 +132,16 @@ class _HomePageState extends State<HomePage> {
   void onTabTapped(int index) {
     setState(() {
       _currentIndex = index;
+      _selectedIndex = index;
+      _selectedDestination = null;
     });
+  }
+
+  void sideBarDestination(int selected) {
+    setState(() {
+      _selectedDestination = selected - 3;
+      _selectedIndex = selected;
+    });
+    Navigator.pop(context);
   }
 }
--- lib/src/welcome/repositories/WelcomePage.dart
+++ lib/src/welcome/repositories/WelcomePage.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:myownmenu/src/configuration/repositories/ConfigurationPage.dart';
-import 'package:myownmenu/src/dispense/repositories/DispensePage.dart';
-import 'package:myownmenu/src/ingredient/repositories/IngredientPage.dart';
-import 'package:myownmenu/src/login/repositories/LoginPage.dart';
-import 'package:myownmenu/src/profile/repositories/ProfilePage.dart';
 import 'package:myownmenu/utils/SourceUtils.dart';
 
 class Welcome extends StatelessWidget {
@@ -28,73 +23,12 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
-  int _selectedDestination = 0;
-
-  @override
   Widget build(BuildContext context) {
-    //final theme = Theme.of(context);
-    //final textTheme = theme.textTheme;
     String _username = 'Usuário';
     Size _size = MediaQuery.of(context).size;
     double _widthPercentage = 0.90;
     double _heightPercentage = 0.17;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        shadowColor: Colors.white12,
-        iconTheme: IconThemeData(color: Colors.black),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            new Padding(
-                padding: const EdgeInsets.only(top: 90),
-                child: TextButton(
-                  child: const Icon(Icons.arrow_back_ios),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                )),
-            new Padding(
-                padding: const EdgeInsets.only(top: 45, left: 20),
-                child: Column(
-                  children: [
-                    ListTile(
-                      leading: Icon(Icons.featured_play_list),
-                      title: Text('Dispensa'),
-                      selected: _selectedDestination == 0,
-                      onTap: () => selectDestination(0),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.local_cafe),
-                      title: Text('Ingredientes'),
-                      selected: _selectedDestination == 1,
-                      onTap: () => selectDestination(1),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.build),
-                      title: Text('Configurações'),
-                      selected: _selectedDestination == 2,
-                      onTap: () => selectDestination(2),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.account_circle),
-                      title: Text('Perfil'),
-                      selected: _selectedDestination == 3,
-                      onTap: () => selectDestination(3),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.input),
-                      title: Text('Sair'),
-                      selected: _selectedDestination == 4,
-                      onTap: () => selectDestination(4),
-                    ),
-                  ],
-                )),
-          ],
-        ),
-      ),
       body: Column(
         children: [
           Container(
@@ -157,20 +91,6 @@ class _WelcomePageState extends State<WelcomePage> {
               primary: Colors.green),
         ),
       ),
-    );
-  }
-
-  void selectDestination(int index) {
-    final List _options = [
-      Dispense(),
-      Ingredient(),
-      Configuration(),
-      Profile(),
-      Login()
-    ];
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => _options[index]),
     );
   }
 }
