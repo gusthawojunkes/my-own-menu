b18964b8d7f85bfee73fa945050dad9752c4712b
Nathan <nathan.stuber.a@gmail.com>
2021-10-05 22:17:01 -0300

    Finalização dos card de filtro e ingredientes.

--- lib/src/ingredient/repositories/IngredientPage.dart
+++ lib/src/ingredient/repositories/IngredientPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:myownmenu/utils/SourceUtils.dart';
 import 'Dart:convert';
+import 'package:flip_card/flip_card.dart';
 
 class Ingredient extends StatelessWidget {
   const Ingredient({Key? key}) : super(key: key);
@@ -24,101 +25,114 @@ class IngredientPage extends StatefulWidget {
 }
 
 class _IngredientPageState extends State<IngredientPage> {
+  GlobalKey<FlipCardState> cardKey = GlobalKey<FlipCardState>();
+
   @override
   Widget build(BuildContext context) {
+    String filtersJson =
+        '{"filters":[{"name":"Fruta"},{"name":"Carne"},{"name":"Grão"}]}';
+    Map<String, dynamic> mapFilters = jsonDecode(filtersJson);
+    List<dynamic> listFilters = mapFilters['filters'];
+
     String ingredientsJson =
-        '{"ingredients":[{"selected":true},{"selected":false},{"selected":true},{"selected":false},{"selected":true},{"selected":false}]}';
+        '{"ingredients":[{"name":"Abacate","type":"Fruta"},{"name":"Alcatra","type":"Carne"},{"name":"Arroz","type":"Grão"},{"name":"Feijão","type":"Grão"},{"name":"Maça","type":"Fruta"},{"name":"Milho","type":"Grão"}]}';
     Map<String, dynamic> mapIngredients = jsonDecode(ingredientsJson);
-    List<dynamic> list = mapIngredients['ingredients'];
-
-    bool _getVisibility(index) {
-      dynamic item = list[index];
-      return item['selected'];
-    }
-
-    void _toggle(index) {
-      dynamic item = list[index];
-      print("1 -item: " + item.toString());
-      setState(() {
-        Map<String, dynamic> map = {'selected': !item['selected']};
-        item['selected'] = map;
-        print("2 - item: " + item.toString());
-      });
-      print("3 - item: " + item.toString());
-    }
+    List<dynamic> listIngredients = mapIngredients['ingredients'];
 
     Widget cardIngredient(index) {
-      return Visibility(
-          visible: !_getVisibility(index),
-          child: Card(
-              margin: const EdgeInsets.only(bottom: 30),
-              child: InkWell(
-                splashColor: Colors.grey.withAlpha(50),
-                onTap: () {
-                  print('Card Ingredient tapped - ' + index.toString());
-                  _toggle(index);
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(right: 15),
-                      child: Text(
-                        "Ingrediente teste",
-                        style: TextStyle(
-                          height: 4,
-                          fontWeight: FontWeight.bold,
-                        ),
+      return Padding(
+        padding: EdgeInsets.only(top: 10, bottom: 10),
+        child: Container(
+          color: Color.fromRGBO(219, 219, 219, 100),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 18, right: 15, bottom: 5),
+                    child: Text(
+                      listIngredients[index]['name'],
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Image.asset(
-                      SourceUtils.INGREDIENTE_SRC,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 5),
+                    child: Text(
+                      listIngredients[index]['type'],
                     ),
-                  ],
-                ),
-              )));
+                  ),
+                ],
+              ),
+              Image.asset(
+                SourceUtils.INGREDIENTE_SRC,
+              ),
+            ],
+          ),
+        ),
+      );
     }
 
     Widget cardSelect(index) {
-      return Visibility(
-          visible: _getVisibility(index),
-          child: Card(
-              child: InkWell(
-            splashColor: Colors.grey.withAlpha(50),
-            onTap: () {
-              print('Card Selectd tapped - ' + index.toString());
-              _toggle(index);
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                    color: Color.fromRGBO(156, 156, 156, 100),
-                    child: Padding(
-                        padding: EdgeInsets.all(15),
-                        child: TextButton(
-                          child: const Icon(
-                            Icons.check_box,
-                            color: Color.fromRGBO(0, 0, 0, 100),
-                          ),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ))),
-                Container(
-                    child: Padding(
-                        padding: EdgeInsets.all(15),
-                        child: Text(
-                          "Ingrediente teste",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ))),
-              ],
-            ),
-          )));
+      return Padding(
+        padding: EdgeInsets.only(top: 10, bottom: 10),
+        child: Container(
+          color: Color.fromRGBO(219, 219, 219, 100),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                  color: Color.fromRGBO(156, 156, 156, 100),
+                  child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Icon(
+                        Icons.check_box,
+                        color: Color.fromRGBO(0, 0, 0, 100),
+                      ))),
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 10, left: 15, bottom: 5),
+                    child: Text(
+                      listIngredients[index]['name'],
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 5, bottom: 15),
+                    child: Text(
+                      listIngredients[index]['type'],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget filter(int index, int r, int g, int b, double o) {
+      return Card(
+          color: Color.fromRGBO(r, g, b, o),
+          clipBehavior: Clip.antiAlias,
+          child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Image.asset(SourceUtils.LOGO_SRC),
+                  Text(
+                    listIngredients[index]['name'],
+                    style: TextStyle(height: 4, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )));
     }
 
     return Scaffold(
@@ -161,22 +175,17 @@ class _IngredientPageState extends State<IngredientPage> {
           child: new SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Row(
-              children: List.generate(list.length, (index) {
-                return new Card(
-                    clipBehavior: Clip.antiAlias,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        children: [
-                          Image.asset(SourceUtils.LOGO_SRC),
-                          Text(
-                            "Teste",
-                            style: TextStyle(
-                                height: 4, fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                    ));
+              children: List.generate(listFilters.length, (index) {
+                return FlipCard(
+                  speed: 1,
+                  front: Container(
+                    child: filter(index, 255, 255, 255, 100),
+                  ),
+                  back: Container(
+                    child: filter(index, 173, 173, 173, 0),
+                  ),
+                );
+                ;
               }),
             ),
           ),
@@ -184,13 +193,19 @@ class _IngredientPageState extends State<IngredientPage> {
         new Container(
           margin: const EdgeInsets.only(left: 30, right: 30),
           child: new Column(
-            children: List.generate(list.length, (index) {
+            children: List.generate(listIngredients.length, (index) {
               return Padding(
                 padding: EdgeInsets.all(0),
                 child: Column(
                   children: [
-                    cardIngredient(index),
-                    cardSelect(index),
+                    FlipCard(
+                      front: Container(
+                        child: cardIngredient(index),
+                      ),
+                      back: Container(
+                        child: cardSelect(index),
+                      ),
+                    ),
                   ],
                 ),
               );
